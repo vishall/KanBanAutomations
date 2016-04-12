ba5c2234f12dcd9710f8391e12569e9886b64346
vishall <vishal.loke@cognizant.com>
2016-04-12 22:44:20 +0100

    gradle project set up

---
+++ src/test/java/testSmartwatchesUntilDeliveryPage.java
@@ -0,0 +1,43 @@
+package test.java;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * Created by user on 01/03/16.
+ */
+public class testSmartwatchesUntilDeliveryPage {
+
+@Test
+    public void firefoxPhoneTest() {
+
+
+    WebDriver driver = new FirefoxDriver();
+
+    driver.manage().window().maximize();
+    driver.manage().deleteAllCookies();
+    driver.navigate().to("https://bau-ref-merch00.ref.o2.co.uk:9443/upgrade/store/smartwatches/");
+
+
+    driver.findElement(By.partialLinkText("Samsung Gear S2")).click();
+    //driver.manage().deleteAllCookies();
+
+    driver.findElement(By.cssSelector("#deviceDetailsSubmit")).click();
+
+
+
+  //  driver.findElement(By.cssSelector("#header > div:nth-of-type(2) > div:nth-of-type(1) > div > button")).click();
+
+    driver.findElement(By.cssSelector("#shopApp > div:nth-of-type(2) > div > div > div:nth-of-type(1) > div:nth-of-type(1) > div > form > input")).click();
+
+
+    driver.close();
+
+    driver.quit();
+
+
+}
+    }
+
