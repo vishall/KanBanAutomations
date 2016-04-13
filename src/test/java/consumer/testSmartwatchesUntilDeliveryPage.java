f24958953473e5f8826b21586ed9e6a1e918bc7e
vishall <vishal.loke@cognizant.com>
2016-04-13 15:51:22 +0100

    added info message

--- src/test/java/consumer/testSmartwatchesUntilDeliveryPage.java
+++ src/test/java/consumer/testSmartwatchesUntilDeliveryPage.java
@@ -10,34 +10,37 @@ import org.openqa.selenium.firefox.FirefoxDriver;
  */
 public class testSmartwatchesUntilDeliveryPage {
 
-@Test
-    public void firefoxPhoneTest() {
+    @Test
+    public void firefoxSmartwatchTest() {
 
 
-    WebDriver driver = new FirefoxDriver();
-
-    driver.manage().window().maximize();
-    driver.manage().deleteAllCookies();
-    driver.navigate().to("https://bau-ref-merch00.ref.o2.co.uk:9443/upgrade/store/smartwatches/");
-
-
-    driver.findElement(By.partialLinkText("Samsung Gear S2")).click();
-    //driver.manage().deleteAllCookies();
-
-    driver.findElement(By.cssSelector("#deviceDetailsSubmit")).click();
+        WebDriver driver = new FirefoxDriver();
+        try {
+            driver.manage().window().maximize();
+            driver.manage().deleteAllCookies();
+            driver.navigate().to("https://bau-ref-merch00.ref.o2.co.uk:9443/upgrade/store/smartwatches/");
 
 
+            driver.findElement(By.partialLinkText("Samsung Gear S2")).click();
+            //driver.manage().deleteAllCookies();
 
-  //  driver.findElement(By.cssSelector("#header > div:nth-of-type(2) > div:nth-of-type(1) > div > button")).click();
-
-    driver.findElement(By.cssSelector("#shopApp > div:nth-of-type(2) > div > div > div:nth-of-type(1) > div:nth-of-type(1) > div > form > input")).click();
-
-
-    driver.close();
-
-    driver.quit();
+            driver.findElement(By.cssSelector("#deviceDetailsSubmit")).click();
 
 
-}
+            //  driver.findElement(By.cssSelector("#header > div:nth-of-type(2) > div:nth-of-type(1) > div > button")).click();
+
+            driver.findElement(By.cssSelector("#shopApp > div:nth-of-type(2) > div > div > div:nth-of-type(1) > div:nth-of-type(1) > div > form > input")).click();
+
+            System.out.println("firefoxSmartwatchTest passed");
+        } catch (Exception ex) {
+            System.err.println("firefoxSmartwatchTest failed");
+            ex.printStackTrace();
+        } finally {
+            driver.close();
+
+            driver.quit();
+        }
+
     }
+}
 
