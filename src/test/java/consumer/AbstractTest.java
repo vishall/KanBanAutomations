10751211d7502a67a7233ca94fb6abae4610c094
vishall <vishal.loke@cognizant.com>
2016-04-29 18:47:09 +0100

    Corrected test by Vim the great

---
+++ src/test/java/consumer/AbstractTest.java
@@ -0,0 +1,36 @@
+package consumer;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by user on 29/04/16.
+ */
+public abstract class AbstractTest {
+
+    static WebDriver driver;
+
+    @BeforeClass
+    public static void setUp() throws Exception {
+
+        driver = new FirefoxDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+    }
+
+    @BeforeMethod
+    public void setUpBefore() throws Exception {
+        driver.manage().deleteAllCookies();
+    }
+
+    @AfterClass
+    public static void tearDown() throws Exception {
+        driver.close();
+        driver.quit();
+    }
+}
---
+++ src/test/java/consumer/DeliveryPageTest.java
@@ -0,0 +1,62 @@
+package consumer;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+/**
+ * Created by user on 01/03/16.
+ */
+public class DeliveryPageTest extends AbstractTest {
+
+    @Test(dataProvider = "phonesTabletMbbData")
+    public void firefoxPhoneTest(String deviceUrl, String deviceName) {
+        driver.navigate().to("https://bau-ref-merch00.ref.o2.co.uk:9443/upgrade/store/"+ deviceUrl +"/");
+
+        driver.findElement(By.partialLinkText(deviceName)).click();
+        driver.manage().deleteAllCookies();
+
+        driver.findElement(By.cssSelector("#deviceDetailsSubmit")).click();
+        driver.manage().deleteAllCookies();
+        driver.findElement(By.cssSelector("#callToAction")).click();
+
+        driver.findElement(By.cssSelector("#header > div:nth-of-type(2) > div:nth-of-type(1) > div > button")).click();
+        driver.findElement(By.name("securecheckout")).click();
+    }
+
+    @Test(dataProvider = "wearablesData")
+    public void firefoxWearablesTest(String deviceUrl, String deviceName) {
+
+        driver.navigate().to("https://bau-ref-merch00.ref.o2.co.uk:9443/upgrade/store/"+ deviceUrl +"/");
+
+        driver.findElement(By.partialLinkText(deviceName)).click();
+        driver.findElement(By.cssSelector("#deviceDetailsSubmit")).click();
+
+        WebDriverWait wait = new WebDriverWait(driver, 5);
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("[data-qa-gotobasket-link]")));
+        driver.findElement(By.cssSelector("[data-qa-gotobasket-link]")).click();
+
+        driver.findElement(By.name("securecheckout")).click();
+    }
+
+    @DataProvider(name = "wearablesData")
+    public static Object[][] wearablesData() {
+        return new Object[][] {
+                {"smartwatches", "Samsung Gear S2"},
+                {"mobile-broadband", "Huawei 4G In-Car wifi"},
+                {"accessories", "Samsung Galaxy Wireless Charging Plate"},
+                {"fitness-trackers", "Fitbit Blaze"}};
+    }
+
+    @DataProvider(name = "phonesTabletMbbData")
+    public static Object[][] phonesTabletMbbData() {
+        return new Object[][] {
+                {"phones", "Apple iPhone 6"},
+                {"mobile-broadband", "Huawei 4G In-Car wifi"},
+                {"tablets", "Sony Xperia Z4"},
+        };
+    }
+}
+
